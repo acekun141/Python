f3376cf5244b0177fed0adbe1dda8c9931a4a336
Ace Kun <acekun141@gmail.com>
2020-01-16 10:24:11 +0700

    add two_sum

---
+++ data_structures/two_sum.py
@@ -0,0 +1,43 @@
+"""
+Given a sorted array of integers, return the two mumbers such that they add up
+to a specific target. You may assume that each input would have exactly one
+solution, and you may not use the same element twice.
+"""
+
+A = [-2, 1, 2, 4, 7, 11]
+target = 13
+
+# Time Complexity: O(n^2)
+def two_sum_brute_force(A, target):
+    for i in range(len(A)-1):
+        for j in range(i + 1, len(A)):
+            if A[i] + A[j] == target:
+                print(A[i], A[j])
+                return True
+    return False
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def two_sum_hash_table(A, target):
+    ht = dict()
+    for i in range(len(A)):
+        if A[i] in ht:
+            print(ht[A[i]], A[i])
+            return True
+        else:
+            ht[target - A[i]] = A[i]
+    return False
+
+def two_sum(A, target):
+    i = 0
+    j = len(A) - 1
+
+    while i <= j:
+        if A[i] + A[j] == target:
+            print(A[i], A[j])
+            return True
+        elif A[i] + A[j] < target:
+            i += 1
+        else:
+            j -= 1
+    return False
